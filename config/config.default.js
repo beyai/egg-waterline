3371ad5f542476ea0f8f2a5dba8f94c998805f3d
chandre <>
2019-03-08 16:30:12 +0800

    支持更多的数据库适配器，支持子目录模型文件

--- config/config.default.js
+++ config/config.default.js
@@ -7,14 +7,18 @@
  */
 exports.waterline = {
 	app : true,
-	agent : false,
-	mount: 'model', // 模型目录
-	db: { // 数据库配置，内部支持 mongodb , mysql
-		default: {
-		adapter: 'mongodb', // 数据库
-		host: 'localhost',
-		port: 27017,
-		    database: 'test',
-		},
-	}
+    agent : false,
+    mount: 'model', // 模型目录
+    migrate: "safe", 	// 数据迁移 safe|drop|alter
+    defaults: {
+		//模型默认设置
+    },
+    db: { 
+		// 数据库配置
+        default: { //数据库名称
+            adapter: 'sails-mysql', // 数据库适配器，
+            host: 'localhost',
+            database: 'testbase',
+        }
+    }
 };
